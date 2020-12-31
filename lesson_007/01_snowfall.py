a428cde5689a89b52c144abc24d5eb9520d4cfda
Максим <bi4ok23@gmail.com>
2020-12-31 11:26:57 +0300

    lesson 007: нужны правки в 01

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -48,6 +48,8 @@ class Snowflakes:
         for index, flake in enumerate(self.__list_of_flakes):
             if flake.can_fall():
                 self.__list_of_fallen_flakes.append(index)
+                # TODO return будет обрывать цикл при первом же выполнении
+                # TODO попробуйте выполнять его после цикла for
                 return len(self.__list_of_fallen_flakes)
 
     def append_flakes(self, count):
@@ -57,6 +59,7 @@ class Snowflakes:
         return self.__list_of_flakes
 
     def delete_flakes(self):
+        # TODO не забывайте проблему сдвигов, удалять надо элементы, начиная с большего
         for index in self.__list_of_fallen_flakes:
             del self.__list_of_flakes[index]
         self.__list_of_fallen_flakes.clear()
